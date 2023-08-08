b478d32a516476bba138875bfab07fec866d8f3c
abel <abelandrade677@gmail.com>
2023-08-08 00:06:24 -0500

    update

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,10 +4,12 @@ import {
   DB_DATABASE,
   DB_HOST,
   DB_PASSWORD,
+  DB_PORT,
   DB_USERNAME,
 } from "../config/config.js";
 
 export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_CONNECTION,
+  port: DB_PORT
 });
