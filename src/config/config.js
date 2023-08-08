7c009f0ef6f9bd0ec2a539107ca0b7127a810ff0
abel <abelandrade677@gmail.com>
2023-08-07 23:42:41 -0500

    update

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,15 @@
 // dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
-export const DB_CONNECTION = process.env.DB_CONNECTION || 'mysql';
+
 export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_DATABASE = process.env.DB_DATABASE || 'practica';
 export const DB_USERNAME = process.env.DB_USERNAME || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_DATABASE = process.env.DB_DATABASE || 'practica';
+export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_CONNECTION = process.env.DB_CONNECTION || 'mysql';
+
+
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'abel-ito-andrade';
 export const REFRESH_TOKEN = process.env.REFRESH_TOKEN || 'abel-ito-andrade-mejia';
 export const MODE = process.env.MODE || 'DEVELOPER';
