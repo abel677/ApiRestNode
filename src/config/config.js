7ae393efa1080fedd820f30bcfa10181e38d9131
abel <abelandrade677@gmail.com>
2023-08-07 23:29:35 -0500

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
-export const PORT = process.env.PORT;
-export const DB_CONNECTION = process.env.DB_CONNECTION;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PORT = process.env.DB_PORT;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_USERNAME = process.env.DB_USERNAME;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const TOKEN_SECRET = process.env.TOKEN_SECRET;
-export const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-export const MODE = process.env.MODE;
+export const PORT = process.env.PORT || 5000;
+export const DB_CONNECTION = process.env.DB_CONNECTION || 'mysql';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_DATABASE = process.env.DB_DATABASE || 'practica';
+export const DB_USERNAME = process.env.DB_USERNAME || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'abel-ito-andrade';
+export const REFRESH_TOKEN = process.env.REFRESH_TOKEN || 'abel-ito-andrade-mejia';
+export const MODE = process.env.MODE || 'DEVELOPER';
