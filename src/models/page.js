522528e20614e58bbfa85ce641c973a178764ff2
abel <abelandrade677@gmail.com>
2023-08-26 14:35:25 -0500

    update timestamps

--- src/models/page.js
+++ src/models/page.js
@@ -1,25 +1,31 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../utils/db.js";
 
-export const Page = sequelize.define('pages', {
+export const Page = sequelize.define(
+  "pages",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     menu: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     page: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     link: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
-})
+  },
+  {
+    timestamps: false,
+  }
+);
--- src/models/pageRole.js
+++ src/models/pageRole.js
@@ -3,21 +3,27 @@ import { sequelize } from "../utils/db.js";
 import { Page } from "./page.js";
 import { Rol } from "./rol.model.js";
 
-export const PageRol = sequelize.define("pagesRoles", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+export const PageRol = sequelize.define(
+  "pagesRoles",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pageId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rolId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  pageId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  rolId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 Page.belongsToMany(Rol, { through: "pagesRoles", foreignKey: "pageId" });
 Rol.belongsToMany(Page, { through: "pagesRoles", foreignKey: "rolId" });
