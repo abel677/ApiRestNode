dc0193193cec921ffa9ef0399024f2ede7393999
abel <abelandrade677@gmail.com>
2023-08-26 14:59:51 -0500

    update port

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import app from "./app.js";
 import { PORT } from "./config/config.js";
 import { sequelize } from "./utils/db.js";
 
-const _PORT = PORT || 3000;
-
 const main = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync({ force: false });
     console.log(">> Base de datos conectada <<");
-    app.listen(_PORT, () => {
-      console.log(`>> Servidor corriendo en el puerto [${_PORT}] <<`);
+    app.listen(PORT, () => {
+      console.log(`>> Servidor corriendo en el puerto [${PORT}] <<`);
     });
   } catch (error) {
     console.log(`Error ${error}`);
